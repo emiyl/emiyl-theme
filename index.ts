a09840a1dfe30bbca908efd03aacbf7728dc27de
emiyl <me@emiyl.com>
2022-05-08 15:55:16 +0100

    fix extra_contributors

--- index.ts
+++ index.ts
@@ -9,6 +9,7 @@ export const localTheme = (options: DefaultThemeOptions): Theme => {
     extends: defaultTheme(options),
     alias: {
       '@theme/Page.vue': path.resolve(__dirname, './components/Page.vue'),
+      '@theme/PageMeta.vue': path.resolve(__dirname, './components/PageMeta.vue'),
       '@theme/Home.vue': path.resolve(__dirname, './components/Home.vue'),
       '@theme/HomeHero.vue': path.resolve(__dirname, './components/HomeHero.vue'),
     },
