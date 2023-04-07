1075bff5119462c2273b71040fe280dabea1a61f
emiyl <me@emiyl.com>
2023-04-07 10:45:08 +0100

    fill in dark/light mode buttons to make more consistent with the other icons

--- index.ts
+++ index.ts
@@ -9,11 +9,12 @@ export const localTheme = (options: DefaultThemeOptions): Theme => {
     extends: defaultTheme(options),
     alias: {
       '@theme/AutoLink.vue': path.resolve(__dirname, './components/AutoLink.vue'),
-      '@theme/Page.vue': path.resolve(__dirname, './components/Page.vue'),
-      '@theme/PageMeta.vue': path.resolve(__dirname, './components/PageMeta.vue'),
       '@theme/Home.vue': path.resolve(__dirname, './components/Home.vue'),
       '@theme/HomeHero.vue': path.resolve(__dirname, './components/HomeHero.vue'),
       '@theme/NavbarDropdown.vue': path.resolve(__dirname, './components/NavbarDropdown.vue'),
+      '@theme/Page.vue': path.resolve(__dirname, './components/Page.vue'),
+      '@theme/PageMeta.vue': path.resolve(__dirname, './components/PageMeta.vue'),
+      '@theme/ToggleColorModeButton.vue': path.resolve(__dirname, './components/ToggleColorModeButton.vue')
     },
     layouts: {
       Layout: path.resolve(__dirname, 'layouts/Layout.vue'),
